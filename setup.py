70edaa568d3e60960c162e3855ddec52d6076115
Ben Cook <bcook@cfa.harvard.edu>
2018-03-30 11:36:45 -0400

    acronym v0.2.0, with corrected pip installation that adds acronym.py to executable path

--- setup.py
+++ setup.py
@@ -3,18 +3,46 @@
 
 # To use: python setup.py install
 
-from setuptools import setup
+import os
+
+from setuptools import setup, Command
+from setuptools.command.install import install
+
+
+class CustomInstall(install):
+    """
+    """
+    def run(self):
+        install.run(self)
+
+
+class CleanCommand(Command):
+    
+    """Custom clean command to tidy up the project root.
+    From https://stackoverflow.com/questions/3779915"""
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
 
 
 setup(
     name='acronym',
-    version='0.1.0',
+    version='0.2.0',
     author='Ben Cook',
     author_email='bcook@cfa.harvard.edu',
     packages=['acronym'],
     url='https://github.com/bacook17/acronym',
     license='LICENSE',
     description="""ACRONYM (Acronym CReatiON for You and Me)""",
-    scripts=['acronym/acronym.py'],
     install_requires=['numpy', 'tqdm', 'pyenchant'],
+    include_package_data=True,
+    scripts=['acronym/acronym.py'],
+    cmdclass={'clean': CleanCommand, 'install': CustomInstall}
 )
